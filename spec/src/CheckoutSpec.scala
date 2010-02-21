584431be10b1f851e9565e198b00174563638c8f
Kerry Buckley <kerryjbuckley@gmail.com>
2010-02-21 22:40:29 +0000

    Extract shared spec

--- spec/src/CheckoutSpec.scala
+++ spec/src/CheckoutSpec.scala
@@ -4,10 +4,7 @@ import org.scalatest.OneInstancePerTest
 
 class CheckoutSpec extends Spec with ShouldMatchers with OneInstancePerTest {
   describe ("Checkout") {
-    describe ("with simple pricing rules") {
-      val rules = new SimplePricingRules(Map("A" -> 50, "B" -> 30))
-      val checkout = new Checkout(rules)
-
+    def basicPricing(checkout: Checkout) = {
       it ("calculates the price of no items as 0") {
         checkout.total should equal(0)
       }
@@ -30,30 +27,18 @@ class CheckoutSpec extends Spec with ShouldMatchers with OneInstancePerTest {
       }
     }
 
+    describe ("with simple pricing rules") {
+      val rules = new SimplePricingRules(Map("A" -> 50, "B" -> 30))
+      val checkout = new Checkout(rules)
+
+      it should behave like basicPricing(checkout)
+    }
+
     describe ("with discount pricing rules") {
       val rules = new DiscountPricingRules(Map("A" -> 50, "B" -> 30))
       val checkout = new Checkout(rules)
 
-      it ("calculates the price of no items as 0") {
-        checkout.total should equal(0)
-      }
-
-      it ("calculates the price of a single item") {
-        checkout.scan("A")
-        checkout.total should equal(50)
-      }
-
-      it ("calculates the price of two identical items") {
-        checkout.scan("A")
-        checkout.scan("A")
-        checkout.total should equal(100)
-      }
-
-      it ("calculates the price of two different items") {
-        checkout.scan("A")
-        checkout.scan("B")
-        checkout.total should equal(80)
-      }
+      it should behave like basicPricing(checkout)
     }
   }
 }
