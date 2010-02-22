a255646bc53b274f66e3eaa2270ce5df6a67f8e1
Kerry Buckley <kerryjbuckley@gmail.com>
2010-02-22 20:11:49 +0000

    Extract helper method for specs

--- spec/src/CheckoutSpec.scala
+++ spec/src/CheckoutSpec.scala
@@ -4,26 +4,26 @@ import org.scalatest.OneInstancePerTest
 
 class CheckoutSpec extends Spec with ShouldMatchers with OneInstancePerTest {
   describe ("Checkout") {
+    def totalPrice(checkout: Checkout, items: String) = {
+      items.toArray.foreach(c => checkout.scan(c.toString))
+      checkout.total
+    }
+
     def basicPricing(checkout: Checkout) = {
       it ("calculates the price of no items as 0") {
-        checkout.total should equal(0)
+        totalPrice(checkout, "") should equal(0)
       }
 
       it ("calculates the price of a single item") {
-        checkout.scan("A")
-        checkout.total should equal(50)
+        totalPrice(checkout, "A") should equal(50)
       }
 
       it ("calculates the price of two identical items") {
-        checkout.scan("A")
-        checkout.scan("A")
-        checkout.total should equal(100)
+        totalPrice(checkout, "AA") should equal(100)
       }
 
       it ("calculates the price of two different items") {
-        checkout.scan("A")
-        checkout.scan("B")
-        checkout.total should equal(80)
+        totalPrice(checkout, "AB") should equal(80)
       }
     }
 
